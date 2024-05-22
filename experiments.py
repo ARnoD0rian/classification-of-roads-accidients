67f2f8bc2fc3753875e791b122fce3d4815723ee
Victor <kolos.130278@gmail.com>
2024-05-22 14:12:21 +0300

    it add logistic regression modifications and it show this algoritm is worse than desision tree for our task

--- experiments.py
+++ experiments.py
@@ -1,14 +1,4 @@
-from sklearn.neighbors import KDTree
 import numpy as np
 
-# Создание массива данных
-X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-
-# Строим KD-дерево
-kdt = KDTree(X, leaf_size=30, metric='euclidean')
-
-# Поиск ближайших соседей
-distances, indices = kdt.query(X, k=2, return_distance=True)
-
-print(distances)
-print(indices)
+a = [1, 2, 3]
+print(a[-3])
